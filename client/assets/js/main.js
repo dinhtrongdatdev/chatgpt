1da62a481d4a2390f8d484fbbe535ff1615e97f1
dinhtrongdatdev <124592551+dinhtrongdatdev@users.noreply.github.com>
2023-02-07 22:05:26 +0700

    Update main.js

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server -> bot's response
 
-  const response = await fetch('https://botnhanh-com.onrender.com', {
+  const response = await fetch('https://chatgpt-ikpf.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
